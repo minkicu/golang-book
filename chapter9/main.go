6415a32ca7e965171f6f8d26ab93c8bf2c15c1e8
minkicu <krissada.r@hotmail.com>
2018-01-15 15:12:58 +0700

    update
    Update

--- chapter9/main.go
+++ chapter9/main.go
@@ -6,6 +6,23 @@ type Circle struct {
 	x, y, r float64
 }
 
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+type Person struct {
+	Name string
+}
+
+type Android struct {
+	Person
+	Model string
+}
+
+func (p *Person) Talk() {
+	fmt.Println("Hi, my name is", p.Name)
+}
+
 func distance(x1, y1, x2, y2 float64) float64 {
 	a := x2 - x1
 	b := y2 - y1
@@ -30,6 +47,17 @@ func pcircleArea(c *Circle) float64 {
 	return math.Pi * c.r*c.r
 }
 
+
+func (c *Circle) mcircleArea() float64 {
+	return math.Pi * c.r*c.r
+}
+
+func (r *Rectangle) mrectangleArea() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return l * w
+}
+
 func main() {
 	var rx1, ry1 float64 = 0, 0
 	var rx2, ry2 float64 = 10, 10
@@ -39,11 +67,11 @@ func main() {
 
 	var c Circle
 	c = Circle{0, 0, 10}
-	fmt.Println(c.x, c.y, c.r)
+	//fmt.Println(c.x, c.y, c.r)
 	fmt.Println(newcircleArea(c))
 
 	d := new(Circle) //default 0
-	fmt.Println(d.x,d.y,d.r)
+	//fmt.Println(d.x,d.y,d.r)
 	d.x=0
 	d.y=0
 	d.r=20
@@ -51,5 +79,19 @@ func main() {
 	
 	e := Circle{x: 0, y: 0, r: 20}
 	fmt.Println(pcircleArea(&e))
+
+	f := Circle{0,0,20}
+	fmt.Println(f.mcircleArea())
+
+	g := Rectangle{0,0,10,10}
+	fmt.Println(g.mrectangleArea())
+
+	h := Person{"New"}
+	h.Talk()
+
+
+	a := new(Android)
+	a.Person.Talk()
+	a.Talk()
 
 }
